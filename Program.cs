8e69556abb490ed17ead005b94ba0ffbfffd5999
DuongNguyen999 <165481404+DuongNguyen999@users.noreply.github.com>
2024-04-11 20:15:18 +0900

    Add du an
    Add du an

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so hang:");
+        int rows = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Nhap so cot:");
+        int cols = int.Parse(Console.ReadLine());
+
+        double[,] matrix = new double[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"Nhap gia tri cho phan tu [{i},{j}]: ");
+                matrix[i, j] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        double maxElement = matrix[0, 0];
+        int maxRow = 0;
+        int maxCol = 0;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (matrix[i, j] > maxElement)
+                {
+                    maxElement = matrix[i, j];
+                    maxRow = i;
+                    maxCol = j;
+                }
+            }
+        }
+        Console.WriteLine($"Phan tu lon nhat la {maxElement}.");
+        Console.WriteLine($"Vi tri lon nhat la [{maxRow},{maxCol}].");
+    }
+}
